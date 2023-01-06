ed9b123feb24c3d126a22aa959b06e2a8fb2dc5f
Raj Mohan <68589327+rajmohanreddy774@users.noreply.github.com>
2023-01-06 14:28:32 +0530

    updating with minor changes on edit and save

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '7c08833c84298bd4ece9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbe324706001c700df60');
